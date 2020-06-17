c58b161a21b94f470fdcd977d0a7d496cc754acc
Muhammad Tauqir Hassnain <64809369+TauqirHassnain@users.noreply.github.com>
2020-06-17 23:47:49 +0500

    5th Assignment add

---
+++ 5th Assingment/JS/app.js
@@ -0,0 +1,184 @@
+// ===== Chapter #1 Task #1
+
+// alert("Welcome to JavaScript Course")
+
+// Task #2
+
+// alert("Error! Please enter a valid password")
+
+// Task #3
+
+// alert("Wlecome to JS Land \nHappy Coding!")
+
+//Task #4
+
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+// ==== Chapter #2 Task#1
+
+// var a = username
+
+// Task #2
+
+// var myName = "Muhammad Tauqir Hassnain"
+
+// Task #3
+
+// var message = "Hello World"
+// alert(message)
+
+// Task #4
+
+// var Sname = prompt("Enter Your Name");
+// var age = prompt("Enter Your Age");
+// var course = prompt ("Enter Your Course Name", "Certified Mobile Application Development");
+// alert(Sname);
+// alert(age);
+// alert(course);
+
+// Task #5
+
+// alert("PIZZA \nPIZZ \nPIZ \nPI \nP ")
+
+// Task#6
+
+// var book = "A smarter way to learn JavaScript."
+// alert(book)
+
+// Task #7
+
+// document.write ("Yah! I can write HTML content through JavaScript")
+
+// // Task #8
+
+// var string = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(string)
+
+// ==== Chapter 3 Task#1
+
+// var age = 24;
+// alert("I am " + age + " years old")
+
+// Task #2 (skipped because still in learning process)
+
+// Task #3
+
+// var birthyear = 1966;
+// document.write("My birth year is " + birthyear)
+
+// Task #4
+
+// var visitor = prompt("Enter Your Name");
+// var ptitle = prompt("Type the product name which you want from us");
+// var quantity = prompt ("How many quantity you want of selected product");
+// document.write(visitor + " ordered" + "  " +  quantity + "  " +  ptitle + " xyz store");
+
+
+// ==== Chapter 4 Task#1
+
+// var a = "variable 01" , b = "Variale 02", C "Variable 03"
+
+// Task #2 (Skipped)
+
+// Task #3
+
+// document.write("<h1> Rules for naming JS variables </h1>");
+// document.write("Variable name can only contain letters, number, $ and _. For example: $my_1stVariable <br>");
+// document.write("Variable names must only begin with letter, $ and _. For Example: name, $name or _name <br>");
+// document.write("Variable Names are case sensitive <br> Variable Names should not be JS keywords");
+
+// ==== Chapter 5 Task#1
+
+// var a = +prompt("Enter Your Number")
+// var b = +prompt("Enter Your Second Number")
+// var c = a+b;
+// document.write ("Sum of " + +a + " and " + +b + " is " + c) 
+
+// Task #2 
+
+// var a = +prompt("Enter Your Number")
+// var b = +prompt("Enter Your Second Number")
+
+// var c = a-b; //Substraction
+// var c = a*b; //Multiplication
+// var c = a/b; //Division
+// var c = a%b; //modulus
+
+// Task #3 
+
+// Task # 4
+
+// Task # 5
+
+// var a = 600;
+// var b = +prompt("Ticket cost is RS 600, how many tickets you want to purchase")
+// var c = a*b;
+// document.write("Total cost to buy " + b + " tickets to a movie is " + c + "PKR");
+
+// Task #7
+
+
+
+//Task #8
+
+// var a= +prompt("Enter obtain marks");
+// var b= +prompt("Enter the total marks");
+// var c = a/b*100;
+// document.write("<h1> Marksheet </h2>");
+// document.write("<br> Obtained Marks is : " + a);
+// document.write("<br> Total Marks is : " + b);
+// document.write("<br> Percentage : " + c + "%")
+
+// Task #9
+
+// // var a=10, b=25;
+// // var c= a*104.80+b*28;
+// document.write("Total Currency in PKR : " + c)
+
+// Task #10
+
+// var a = +prompt("Type any number");
+// var b = a+5*10/2;
+// document.write("")
+// document.write("Entered number after calculation is (add 5, Multiply 10, Divide 2) : " + b);
+
+// Task #11
+
+// var a = +prompt("Enter the current Year");
+// var b = +prompt("Enter the birth Year");
+// var c= a-b;
+// document.write("<h1>Age Calculator</h2>" )
+// document.write("<br>Current Year is : " + a);
+// document.write("<br>Birth Year is : " + b);
+// document.write("<br>Your age is : " + c);
+
+// Task #13
+
+// var a = prompt ("Enter your favourite Snack");
+// var b = +prompt ("Enter your current Age");
+// var c = +prompt ("Enter your maximun estimate life")
+// var d = +prompt ("Enter the estimated snack amount per day");
+// var e = (c-b)*d;
+// document.write("<h1> The Lifetime Supply Calculator </h1>")
+// document.write("<br> Favourite Snack : " + a);
+// document.write("<br> Current Age : " + b);
+// document.write("<br> Estimated Maximun Age : " + c);
+// document.write("<br> Amount of snacks per day : " + d);
+// document.write("<br> You will need " + e + " to last you until the ripe old age of " + c);
+
+
+// ==== Chapter 6-9 Task#1
+
+var a= +prompt("Enter any number")
+var b = ++a;
+document.write("<h2>Result</h2>")
+document.write("<br> The value of a is : " + a)
+document.write("<br> The Value of ++a is " + b)
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
